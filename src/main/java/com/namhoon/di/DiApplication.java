28ef0089992bb8b7f0212e0311b85165536811f9
i-kemen <lynshu19@gmail.com>
2023-09-13 16:02:05 +0900

    스프링 이전

---
+++ src/main/java/com/namhoon/di/DiApplication.java
@@ -0,0 +1,15 @@
+package com.namhoon.di;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+
+@SpringBootApplication
+public class DiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiApplication.class, args);
+    }
+
+}
